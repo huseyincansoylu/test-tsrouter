0fb0bbb5d0b56d71ff0df638c86d2af322d154cf
hcsoylu <hcsoylu.dev@gmail.com>
2025-04-10 18:51:05 +0300

    navigation improvements

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,52 @@
-import { Outlet, createRootRouteWithContext } from "@tanstack/react-router"
+import {
+  Link,
+  Outlet,
+  createRootRouteWithContext,
+  useRouter,
+} from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 
-import type { AuthContext } from "../auth"
+import { useAuth, type AuthContext } from "../auth"
+import { Button } from "@/components/ui/button"
+import { useReducer } from "react"
 
 interface MyRouterContext {
   auth: AuthContext
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  component: () => (
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const auth = useAuth()
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    await auth.logout()
+    router.invalidate()
+  }
+
+  return (
     <>
+      <div className="h-12 p-8 flex items-center justify-between border-b border-gray-400">
+        <Link to="/">Nevermined</Link>
+
+        <div>
+          <Link to="/dashboard">Dashboard</Link>
+        </div>
+
+        {auth.isAuthenticated ? (
+          <Button onClick={handleLogout}>Log out</Button>
+        ) : (
+          <Button asChild>
+            <Link to="/login">Log in</Link>
+          </Button>
+        )}
+      </div>
+
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" initialIsOpen={false} />
     </>
-  ),
-})
+  )
+}
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,25 +1,14 @@
-import * as React from "react"
-import {
-  createFileRoute,
-  redirect,
-  useRouter,
-  useRouterState,
-} from "@tanstack/react-router"
-import { z } from "zod"
+import { createFileRoute, redirect, useRouter } from "@tanstack/react-router"
 
 import { useAuth } from "../auth"
-import { sleep } from "@/lib/utils"
 
-// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-const fallback = "/dashboard" as const
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export const Route = createFileRoute("/login")({
-  validateSearch: z.object({
-    redirect: z.string().optional().catch(""),
-  }),
-  beforeLoad: ({ context, search }) => {
+  beforeLoad: ({ context }) => {
     if (context.auth.isAuthenticated) {
-      throw redirect({ to: search.redirect || fallback })
+      throw redirect({ to: "/dashboard" })
     }
   },
   component: LoginComponent,
@@ -28,51 +17,58 @@ export const Route = createFileRoute("/login")({
 function LoginComponent() {
   const auth = useAuth()
   const router = useRouter()
-  const isLoading = useRouterState({ select: (s) => s.isLoading })
-  const navigate = Route.useNavigate()
-  const [isSubmitting, setIsSubmitting] = React.useState(false)
 
-  const search = Route.useSearch()
-
-  const onFormSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
-    setIsSubmitting(true)
+  const handleLogin = async (
+    provider: "google" | "github" | "facebook" | "reddit"
+  ) => {
     try {
-      evt.preventDefault()
-    
-      await auth.login()
-
+      await auth.login(provider)
       await router.invalidate()
 
-      await sleep(1)
-
-      await navigate({ to: search.redirect || fallback })
-    } catch (error) {
-      console.error("Error logging in: ", error)
-    } finally {
-      setIsSubmitting(false)
+      await router.navigate({ to: "/dashboard" })
+    } catch (err) {
+      console.error("Login failed", err)
     }
   }
 
-  const isLoggingIn = isLoading || isSubmitting
-
   return (
-    <div className="p-2 grid gap-2 place-items-center">
-      <h3 className="text-xl">Login page</h3>
-      {search.redirect ? (
-        <p className="text-red-500">You need to login to access this page.</p>
-      ) : (
-        <p>Login to see all the cool content in here.</p>
-      )}
-      <form className="mt-4 max-w-lg" onSubmit={onFormSubmit}>
-        <fieldset disabled={isLoggingIn} className="w-full grid gap-2">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-4 rounded-md w-full disabled:bg-gray-300 disabled:text-gray-500"
+    <div className="grid gap-6 max-w-[420px] mx-auto mt-40">
+      <div className="grid gap-4 place-items-center">
+        <Button className="w-full" onClick={() => handleLogin("google")}>
+          Login with Google
+        </Button>
+
+        <div className="grid grid-cols-3 gap-4 w-full">
+          <Button
+            className="w-full"
+            variant="secondary"
+            onClick={() => handleLogin("github")}
           >
-            {isLoggingIn ? "Loading..." : "Login"}
-          </button>
-        </fieldset>
-      </form>
+            Github
+          </Button>
+
+          <Button
+            className="w-full"
+            variant="secondary"
+            onClick={() => handleLogin("facebook")}
+          >
+            Facebook
+          </Button>
+
+          <Button
+            className="w-full"
+            variant="secondary"
+            onClick={() => handleLogin("reddit")}
+          >
+            Reddit
+          </Button>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <Input placeholder="Email/Phone" />
+
+        <Button className="bg-purple-400 hover:bg-purple-300">Continue</Button>
+      </div>
     </div>
   )
 }
